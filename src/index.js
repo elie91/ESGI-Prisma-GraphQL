63da2e6f499514fbc46e8b0e02d95c38a093d98d
Elie Bismuth <eliebismuth91@gmail.com>
2020-10-07 16:25:26 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const {GraphQLServer} = require('graphql-yoga')
+const posts = [
+    {
+        id: 1,
+        title: 'Article one',
+        content: 'This is Article One'
+    },
+    {
+        id: 2,
+        title: 'Article Two',
+        content: 'This is Article Two'
+    },
+    {
+        id: 3,
+        title: 'Article Three',
+        content: 'This is Article Three'
+    }
+];
+const typeDefs = `
+    type Query {
+        post(postId: Int!): Post
+        allPosts: [Post!]!
+    }
+    type Mutation {
+        createPost(title: String!, content: String!): Post!
+    }
+    type Post {
+         id: Int
+         title: String
+         content: String
+    }
+  `
+
+const resolvers = {
+    Query: {
+        post: (_, {postId}, context, info) => {
+            return posts.filter(post => post.id === postId)[0];
+        },
+        allPosts: (_, args, context, info) => {
+            return posts;
+        }
+    },
+    Mutation: {
+        createPost(_, args, context, info) {
+            const newPost = {
+                id: posts.length + 1,
+                title: args.title,
+                content: args.content
+            }
+            posts.push(newPost)
+            return newPost
+        }
+    }
+}
+const server = new GraphQLServer({typeDefs, resolvers})
+server.start(() => console.log('Server is running on localhost:4000'))
